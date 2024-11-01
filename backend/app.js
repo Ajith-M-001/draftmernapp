d64313a40dcaf5b80e2a401d3aa09f07145e40cb
Ajith-M-001 <ajithguru741@gmail.com>
2024-11-01 08:15:24 +0530

    backend server started

---
+++ backend/app.js
@@ -0,0 +1,24 @@
+import express from "express";
+import dotenv from "dotenv";
+
+// Load environment variables
+dotenv.config();
+
+const app = express();
+const port = process.env.PORT || 3000;
+
+// Middleware
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+// Basic route
+app.get("/", (req, res) => {
+  res.send("Hello World!");
+});
+
+// Start server
+app.listen(port, () => {
+  console.log(`Server is running on port http://localhost:${port}`);
+});
+
+export default app;
